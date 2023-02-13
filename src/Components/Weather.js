f8673b6867086a8a5fedaf40891859a7923b43cb
Muhammed Emin Ayar <eminayar142000@gmail.com>
2023-02-13 14:43:11 +0300

    second commit

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -6,13 +6,13 @@ import Today from "./Today"
 function Weather() {
     const {search,weather,setWeather} = useContext(WeatherContext);
     useEffect(() => {
-        // fetch(`https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=${search}`,{
-        //     method: 'GET',
-        //     headers: {
-        //         "content-type": "application/json",
-        //         "authorization" : "apikey 2QpYkTcDy1jBGM8Ohm743o:1ddiCRnf2BQIm6HZZENzT8"
-        //     }
-        // }).then(res => res.json()).then(data => setWeather(data.result));
+        fetch(`https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=${search}`,{
+            method: 'GET',
+            headers: {
+                "content-type": "application/json",
+                "authorization" : "apikey 2QpYkTcDy1jBGM8Ohm743o:1ddiCRnf2BQIm6HZZENzT8"
+            }
+        }).then(res => res.json()).then(data => setWeather(data.result));
     },[search])
     console.log(weather);
   return (
